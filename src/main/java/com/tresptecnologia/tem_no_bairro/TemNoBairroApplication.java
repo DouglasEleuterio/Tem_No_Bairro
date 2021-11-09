f5ba7903f17371841e3a598c2b5b3054b84a85d4
Douglas <douglasferreira@hdregistros.com.br>
2021-11-09 16:10:38 -0300

    first commit

---
+++ src/main/java/com/tresptecnologia/tem_no_bairro/TemNoBairroApplication.java
@@ -0,0 +1,13 @@
+package com.tresptecnologia.tem_no_bairro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemNoBairroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TemNoBairroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tresptecnologia/tem_no_bairro/TemNoBairroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tresptecnologia.tem_no_bairro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemNoBairroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
